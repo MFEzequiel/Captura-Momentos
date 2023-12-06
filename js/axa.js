ba42985216bdaddcd5565503a4620bcc75713965
Marcelo <marceloexequielf4@gmail.com>
2023-12-06 18:14:28 -0300

    new cambios

--- js/axa.js
+++ js/axa.js
@@ -1,19 +1,8 @@
-const requestData = {
-    method : 'getUsers'
-};
-const usersPromise = fetch('/api', {
-    method : 'POST',
-    body : JSON.stringify(requestData)
-}).then(response => {
-    if (!response.ok) {
-    throw new Error("Got non-2XX response from API server.");
-    }
-    return response.json();
-}).then(responseData => {
-    return responseData.users;
-});
-usersPromise.then(users => {
-    console.log("Known users: ", users);
-}, error => {
-    console.error("Failed to fetch users due to error: ", error);
+var output = document.createElement('p');
+document.body.appendChild(output);
+output.textContent = localStorage.getItem('user-value');
+window.addEventListener('storage', function(event) {
+ if (event.key === 'user-value') {
+ output.textContent = event.newValue;
+ }
 });
--- js/main.js
+++ js/main.js
@@ -1,11 +1,59 @@
 const d = document;
+// boton de hamburgesa
 const bt = d.querySelector('.bt');
 const dv = d.querySelectorAll('.d');
+// navegación del header
 const nv = d.querySelector('.hnav');
+const ul = d.querySelector('.menu');
+// cuadro de código
+const code = d.querySelectorAll('.code');
+const btCode = d.querySelectorAll('.icon');
+let index = 0;
 
-bt.addEventListener('click', ()=>{
-  for(let i =0; i<dv.length; i++){
-    dv[i].classList.toggle('div')
+
+d.addEventListener('DOMContentLoaded',()=>{
+  bt.addEventListener('click', ()=>{
+    for(let i =0; i<dv.length; i++){
+      dv[i].classList.toggle('div');
+    };
+    nv.classList.toggle('active');
+  });
+  
+  function createNav(link,link1,link2,link3,link4,link5){
+    return `
+    <li><a class="hhnav" href="${link}">Inicio</a></li>
+    <li><a class="hhnav" href="${link1}">HTML</a></li>
+    <li><a class="hhnav" href="${link2}">CSS</a></li>
+    <li><a class="hhnav" href="${link3}">JavaScript</a></li>
+    <li><a class="hhnav" href="${link4}">Python</a></li>
+    <li><a class="hhnav" href="${link5}">Proyectos</a></li>
+    `
   }
-  nv.classList.toggle('active');
+  
+  let link = ul.getAttribute('data-1');
+  let link1 = ul.getAttribute('data-2');
+  let link2 = ul.getAttribute('data-3');
+  let link3 = ul.getAttribute('data-4');
+  let link4 = ul.getAttribute('data-5');
+  let link5 = ul.getAttribute('data-6');
+
+  ul.innerHTML = createNav(link,link1,link2,link3,link4,link5);
+
+  function showCode(){
+    code.forEach((btCodeItems,idx)=>{
+      if (idx === index){
+        btCodeItems.classList.toggle('active');
+      };
+    });
+  };
+
+  function selectCode(){
+    for (let i = 0; i < btCode.length;i++){
+      btCode[i].addEventListener('click', ()=>{
+        index = i % btCode.length;
+        showCode();
+      })
+    }
+  }
+  selectCode();
 });
