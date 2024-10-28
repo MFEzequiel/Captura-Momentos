8e8ac0a5a0e7427b026522004fbb97b32e1adbe6
Ferreyra Marcelo Ezequiel <mareloexequielf4@gmail.com>
2024-10-27 21:38:14 -0300

    mmmmm

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,19 @@
 import { Card } from './Card'
 import { Carousel } from './Carousel'
+import img from '../../public/img/carousel-1.jpg'
+import img1 from "/public/img/foto-6.jpg"
+import img2 from '/public/client/María.jpg'
+import img3 from '/public/client/Carlos.jpg'
+import img4 from '../../public/img/foto-1.jpg'
+import img5 from '../../public/img/foto-2.jpg'
+import img6 from '../../public/img/foto-3.jpg'
+import img7 from '../../public/img/foto-4.jpg'
+import img8 from '../../public/img/foto-6.jpg'
+import img9 from '../../public/img/foto-7.jpg'
+import img10 from '../../public/img/foto-8.jpg'
+import img11 from '../../public/img/foto-9.jpg'
+import img12 from '../../public/img/foto-10.jpg'
+// import img7 from ''
 
 export function Home () {
 
@@ -17,22 +31,22 @@ export function Home () {
           <p>Creemos que una buena fotografía es aquella que te hace revivir el momento una y otra vez.</p>
           <p>Explora nuestras secciones para descubrir nuestro estilo y conocer las imágenes que han dejado huella en nuestros clientes.</p>
           <div className="container-work">
-              <img src="../../public/img/foto-1.jpg" alt="" className="work-image" />
-              <img src="../../public/img/foto-2.jpg" alt="" className="work-image" />
-              <img src="../../public/img/foto-3.jpg" alt="" className="work-image" />
-              <img src="../../public/img/foto-4.jpg" alt="" className="work-image" />
-              <img src="../../public/img/foto-6.jpg" alt="" className="work-image" />
-              <img src="../../public/img/foto-7.jpg" alt="" className="work-image" />
-              <img src="../../public/img/foto-8.jpg" alt="" className="work-image" />
-              <img src="../../public/img/foto-9.jpg" alt="" className="work-image" />
-              <img src="../../public/img/foto-10.jpg" alt="" className="work-image" />
+              <img src={img4} alt="" className="work-image" />
+              <img src={img5} alt="" className="work-image" />
+              <img src={img6} alt="" className="work-image" />
+              <img src={img7} alt="" className="work-image" />
+              <img src={img8} alt="" className="work-image" />
+              <img src={img9} alt="" className="work-image" />
+              <img src={img10} alt="" className="work-image" />
+              <img src={img11} alt="" className="work-image" />
+              <img src={img12} alt="" className="work-image" />
           </div>
         </div>
         <div className="home-service">
           <p className='filterable-title pos-relative'>
             <span className='filterable-title-text title'>Servicios</span>
           </p> 
-          <img src="../../public/img/carousel-1.jpg" alt="" className="services-image" />
+          <img src={img} alt="" className="services-image" />
           <p className="paragraph">Nos especializamos en una variedad de servicios pensados para cumplir con las expectativas de nuestros clientes.</p>
           <p className="paragraph">Cada sesión es cuidadosamente planeada y adaptada para capturar exactamente lo que imaginas.</p>
           <p className="title">Algunos de nuestros servicios incluyen:</p>
@@ -56,9 +70,9 @@ export function Home () {
             <span className='filterable-title-text title'>Clientes</span>
           </p>
           <div className="client-image">
-            <Card image={'/public/img/foto-6.jpg'} name={'Ana y Jorge'} text={'Trabajar con el equipo fue una experiencia increíble. No solo capturaron los mejores momentos de nuestra boda, sino que lo hicieron con tanto cariño y profesionalismo que superaron todas nuestras expectativas.'} />
-            <Card image={'/public/client/María.jpg'} name={'María P.'} text={'No puedo agradecer lo suficiente por las fotos. Como alguien que no está acostumbrado a posar, su equipo hizo que me sintiera tan cómodo y en confianza. Las fotos quedaron espectaculares.'} />
-            <Card image={'/public/client/Carlos.jpg'} name={'Carlos L.'} text={'Ellos no solo capturaron nuestras fotos, capturaron nuestras emociones. Recomendaría al equipo a cualquiera que quiera fotos sinceras y artísticas.'} />
+            <Card image={img1} name={'Ana y Jorge'} text={'Trabajar con el equipo fue una experiencia increíble. No solo capturaron los mejores momentos de nuestra boda, sino que lo hicieron con tanto cariño y profesionalismo que superaron todas nuestras expectativas.'} />
+            <Card image={img2} name={'María P.'} text={'No puedo agradecer lo suficiente por las fotos. Como alguien que no está acostumbrado a posar, su equipo hizo que me sintiera tan cómodo y en confianza. Las fotos quedaron espectaculares.'} />
+            <Card image={img3} name={'Carlos L.'} text={'Ellos no solo capturaron nuestras fotos, capturaron nuestras emociones. Recomendaría al equipo a cualquiera que quiera fotos sinceras y artísticas.'} />
           </div>
         </div>
       </section>
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,4 +1,9 @@
 import { Card } from "./Card";
+import img from '../../public/img/carousel-1.jpg'
+import img1 from '/public/team/Rosa.jpg'
+import img2 from '/public/team/Emma.jpg'
+import img3 from '/public/team/Steve.jpg'
+
 
 export function Nosotros () {
 
@@ -8,7 +13,7 @@ export function Nosotros () {
         <p className='filterable-title pos-relative'>
           <span className='filterable-title-text title'>Historia del estudio</span>
         </p> 
-        <img src="../../public/img/carousel-1.jpg" alt="" className="services-image" />
+        <img src={img} alt="" className="services-image" />
         <div className="about-content">
           <p className="title">Historia del estudio:</p>
           <p className="paragraph">Nuestro estudio comenzó con una simple pero poderosa visión: capturar momentos reales de la vida de las personas. Desde nuestros humildes comienzos en 2017, hemos crecido y evolucionado, ganando la confianza de nuestros clientes a lo largo del camino.</p>
@@ -23,9 +28,9 @@ export function Nosotros () {
         <p className="paragraph">Nuestro equipo está compuesto por profesionales apasionados y talentosos, cada uno con su especialidad, pero todos compartiendo la misma misión: capturar momentos que perduren</p>
         <p className="paragraph">La colaboración y el espíritu de equipo nos definen; cada uno aporta algo especial que enriquece nuestra visión como estudio.</p>
         <div className="client-image">
-            <Card image={'/public/team/Rosa.jpg'} name={'Raphone Rosa'} text={'Con años de experiencia en fotografía de retrato y moda, tiene una habilidad especial para capturar la esencia y el estilo de cada persona que fotografía.'} />
-            <Card image={'/public/team/Emma.jpg'} name={'Emma James'} text={'Enfocado en fotografía de eventos, es experto en captar esos momentos espontáneos y llenos de emoción que hacen que cada evento sea inolvidable.'} />
-            <Card image={'/public/team/Steve.jpg'} name={'Steve Gerrish'} text={'Especialista en fotografía artística y creativa, le encanta experimentar con luz y composición para crear imágenes únicas y visualmente impactantes.'} />
+            <Card image={img1} name={'Raphone Rosa'} text={'Con años de experiencia en fotografía de retrato y moda, tiene una habilidad especial para capturar la esencia y el estilo de cada persona que fotografía.'} />
+            <Card image={img2} name={'Emma James'} text={'Enfocado en fotografía de eventos, es experto en captar esos momentos espontáneos y llenos de emoción que hacen que cada evento sea inolvidable.'} />
+            <Card image={img3} name={'Steve Gerrish'} text={'Especialista en fotografía artística y creativa, le encanta experimentar con luz y composición para crear imágenes únicas y visualmente impactantes.'} />
         </div>
       </div>
         <div className="about-prime">
